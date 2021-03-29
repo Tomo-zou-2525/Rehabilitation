2eaf6cc91dc80194013ff8dc4acdc6beea90d54a
友造-2525 <bathbook1990tomozou@gmail.com>
2021-03-29 20:20:00 +0900

    props 復讐

--- Test2/app.js
+++ Test2/app.js
@@ -1,25 +1,7 @@
-//Vueの構文を宣言
-let app = new Vue({
-  //appをマウント
-  el: "#app",
-  /*Vueインスタンスに対してデータを定義する
-  newItem→todoリストの新規入力値の入れ物
-  todos~→初期に入っているリストの文字列
-  methods→データ駆動時に作動する関数などを定義する場所
-  addItem→追加ボタンでnewItemとして追加される
+Vue.component("the-header", {
+  template: "<p>Test</p>"
+});
 
-
-
-  */
-  data: {
-    newItem: "",
-    todos: [
-      { title: "task 1", isDone: true },
-      { title: "task 2", isDone: true },
-      { title: "task 3", isDone: true }
-    ]
-  },
-  methods: {
-    addItem: function() {}
-  }
+new Vue({
+  el: "#app"
 });
