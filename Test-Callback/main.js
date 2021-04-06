67e30ba0191aae9cbf0f8bd780b60366feda4734
友造-2525 <bathbook1990tomozou@gmail.com>
2021-04-06 22:55:21 +0900

    Promiseの基本的な使い方

--- Test-Callback/main.js
+++ Test-Callback/main.js
@@ -1,5 +1,28 @@
-console.log("111");
+//setTimeout
 
-setTimeout(() => {
-  console.log("unko" + "buriburi");
-}, 5000);
+// console.log("111");
+
+// setTimeout(() => {
+//   console.log("unko" + "buriburi");
+// }, 5000);
+
+// console.log("222");
+
+//Promise
+
+//Promiseオブジェクトを作成
+const promise = new Promise((resolve, reject) => {
+  //resolve, rejectも関数
+  //成功→resolve、失敗→reject
+  setTimeout(() => {
+    console.log("promiseの中のSetTimeout");
+    resolve("なんぞ");
+  }, 2000);
+});
+//promiseオブジェクトにthenメソッドを用意しておくことで
+//thenメソッドの引数にcallback関数を用意しておくと
+//resolve()が呼ばれたタイミングでpromise.thenの中身のcallback関数が呼ばれる
+promise.then((data) => {
+  console.log("thenの中");
+  console.log("data: ", data);
+});
