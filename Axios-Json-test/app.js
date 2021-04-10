0841256b92aeb824d38fd905d5cbd7e6a5729fd8
友造-2525 <bathbook1990tomozou@gmail.com>
2021-04-10 16:42:40 +0900

    jsonファイル記述

---
+++ Axios-Json-test/app.js
@@ -0,0 +1,9 @@
+let app = new Vue({
+  el: "#app",
+  data: {
+    members: [
+      { name: "ともや", age: 30, mail: "unko@buriburi.com" },
+      { name: "ともみ", age: 57, mail: "unko@bitibiti.com" },
+    ],
+  },
+});
