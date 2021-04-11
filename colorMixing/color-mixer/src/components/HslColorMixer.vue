7fae71bd59b29e74fb096610aa95015146ef446c
友造-2525 <bathbook1990tomozou@gmail.com>
2021-04-11 17:52:06 +0900

    カラーパレット、光彩調整機能追加

--- colorMixing/color-mixer/src/components/HslColorMixer.vue
+++ colorMixing/color-mixer/src/components/HslColorMixer.vue
@@ -1,58 +1,179 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="frame box">
+    <span class="title">HSL カラーミキサー</span>
+    <div class="field">
+      <input
+        class="slider is-fullwidth"
+        type="range"
+        min="0"
+        max="360"
+        step="1"
+        v-model.number="hue"
+      />
+      <div>Hue (色相): {{ hue }}</div>
+    </div>
+    <div class="field">
+      <input
+        class="slider is-fullwidth"
+        type="range"
+        min="0"
+        max="100"
+        step="1"
+        v-model.number="saturation"
+      />
+      <div>Saturation (彩度): {{ saturation }}</div>
+    </div>
+    <div class="field">
+      <input
+        class="slider is-fullwidth"
+        type="range"
+        min="0"
+        max="100"
+        step="1"
+        v-model.number="lightness"
+      />
+      <div>Lightness (輝度): {{ lightness }}</div>
+    </div>
+    <div>
+      <input
+        class="input is-large"
+        type="text"
+        readonly
+        v-bind:style="{ 'background-color': hsl }"
+        v-model="colorCode"
+      />
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
+import "../style/bulma/css/bulma.css";
+import "../style/bulma-extensions/dist/css/bulma-extensions.min.css";
 
+export default {
+  name: "HslColorMixer",
+  data() {
+    return {
+      hue: 0,
+      saturation: 0,
+      lightness: 0,
+    };
+  },
+  computed: {
+    // カラーコード
+    colorCode() {
+      return this.toRgbColorCode();
+    },
+    // HSL
+    hsl() {
+      return `hsl(${this.hue}, ${this.saturation}%, ${this.lightness}%)`;
+    },
+  },
+  methods: {
+    // HSL を RGB カラーコードに変換
+    toRgbColorCode() {
+      if (this.lightness === 0) {
+        // 輝度 0
+        return "#000000";
+      }
+      if (this.lightness === 100) {
+        // 輝度 100
+        return "#ffffff";
+      }
+
+      const h = this.hue % 360;
+      const s = this.saturation / 100;
+      const l = this.lightness / 100;
+
+      // 範囲値
+      let range;
+      if (l < 0.5) {
+        range = l;
+      } else {
+        range = 1 - l;
+      }
+
+      // 最大値
+      const max = l + range * s;
+      // 最小値
+      const min = l - range * s;
+
+      // RGB
+      let red;
+      let green;
+      let blue;
+      if (h <= 60) {
+        red = max;
+        green = (h / 60) * (max - min) + min;
+        blue = min;
+      } else if (h <= 120) {
+        red = ((120 - h) / 60) * (max - min) + min;
+        green = max;
+        blue = min;
+      } else if (h <= 180) {
+        red = min;
+        green = max;
+        blue = ((h - 120) / 60) * (max - min) + min;
+      } else if (h <= 240) {
+        red = min;
+        green = ((240 - h) / 60) * (max - min) + min;
+        blue = max;
+      } else if (h <= 300) {
+        red = ((h - 240) / 60) * (max - min) + min;
+        green = min;
+        blue = max;
+      } else {
+        red = max;
+        green = min;
+        blue = ((360 - h) / 60) * (max - min) + min;
+      }
+      // 調整
+      red = Math.floor(red * 255);
+      if (red > 255) {
+        red = 255;
+      } else if (red < 0) {
+        red = 0;
+      }
+      green = Math.floor(green * 255);
+      if (green > 255) {
+        green = 255;
+      } else if (green < 0) {
+        green = 0;
+      }
+      blue = Math.floor(blue * 255);
+      if (blue > 255) {
+        blue = 255;
+      } else if (blue < 0) {
+        blue = 0;
+      }
+      // RGB 16進数
+      const r = this.zeroPadding(this.decimalToHex(red));
+      const g = this.zeroPadding(this.decimalToHex(green));
+      const b = this.zeroPadding(this.decimalToHex(blue));
+      // カラーコード
+      return `#${r}${g}${b}`;
+    },
+    // 10進数から16進数に変換する
+    decimalToHex(value) {
+      return Number(value).toString(16);
+    },
+    // 0パディング（2桁）
+    zeroPadding(value) {
+      return ("00" + value).slice(-2);
+    },
+  },
+};
+</script>
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
+frame {
+  margin: 10px;
+  margin-top: 20px;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.field {
+  margin-bottom: 10px;
+  :last-child {
+    margin-bottom: 0;
+  }
 }
 </style>
--- colorMixing/color-mixer/src/components/RgbColorMixer.vue
+++ colorMixing/color-mixer/src/components/RgbColorMixer.vue
@@ -1,58 +1,116 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="frame box">
+    <span class="title">RGB カラーミキサー</span>
+    <div class="field">
+      <input
+        class="slider is-fullwidth is-danger"
+        type="range"
+        min="0"
+        max="255"
+        step="1"
+        v-model.number="red"
+      />
+      <div>Red(赤): {{ red }}</div>
+    </div>
+    <div class="field">
+      <input
+        class="slider is-fullwidth is-success"
+        type="range"
+        min="0"
+        max="255"
+        step="1"
+        v-model.number="green"
+      />
+      <div>Green (緑): {{ green }}</div>
+    </div>
+    <div class="field">
+      <input
+        class="slider is-fullwidth is-info"
+        type="range"
+        min="0"
+        max="255"
+        step="1"
+        v-model.number="blue"
+      />
+      <div>Blue (青): {{ blue }}</div>
+    </div>
+    <div>
+      <input
+        class="input is-large"
+        type="text"
+        readonly
+        v-bind:style="{ 'background-color': rgb }"
+        v-model="colorCode"
+      />
+    </div>
   </div>
 </template>
 
 <script>
+import "../style/bulma/css/bulma.css";
+import "../style/bulma-extensions/dist/css/bulma-extensions.min.css";
+
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  name: "RgbColorMixer",
+  data() {
+    return {
+      red: 0,
+      green: 0,
+      blue: 0,
+    };
+  },
+  computed: {
+    //Red(赤)１６進数
+    redHex() {
+      let h = this.decimalToHex(this.red);
+      h = this.zeroPadding(h);
+      return h;
+    },
+    //Red(緑)１６進数
+    greenHex() {
+      let h = this.decimalToHex(this.green);
+      h = this.zeroPadding(h);
+      return h;
+    },
+    //Red(青)１６進数
+    blueHex() {
+      let h = this.decimalToHex(this.blue);
+      h = this.zeroPadding(h);
+      return h;
+    },
+    //カラーコード
+    colorCoded() {
+      return `#${this.redHex}${this.greenHex}${this.blueHex}`;
+    },
+    //RGB
+    rgb() {
+      return `rgb(${this.red},${this.green},${this.blue})`;
+    },
+  },
+  methods: {
+    //10進数から16進数に変換
+    decimalToHex(value) {
+      return Number(value).toString(16);
+    },
+    //0パディング(2けた)
+    zeroPadding(value) {
+      return ("00" + value).slice(-2);
+    },
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
+.frame {
+  margin: 10px;
+  margin-top: 20px;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+
+.field {
+  margin-bottom: 10px;
+  :last-child {
+    margin-bottom: 0;
+  }
 }
 </style>
