2ab065231de8b1f8739c85c023359553549f3a31
友造-2525 <bathbook1990tomozou@gmail.com>
2021-04-15 20:36:24 +0900

    clickイベント実装

--- makeTab/app.js
+++ makeTab/app.js
@@ -3,11 +3,25 @@
 (() => {
   const $doc = document
   const $tabItem = $doc.getElementById("js-tab")
+  const $tabData = $tabItem.querySelectorAll('[data-nav]')
   const $tabContent = $tabItem.querySelectorAll('[data-content]')
   //初期化＝最初に行わせたい時に行う
   const init = () => {
     $tabContent[0].style.display = "block"
   }
-  init()
+  init();
+  //クリックしたら起こるイベント
+  //(e)はevent クリックされるなどのイベントについてくる情報
+  const handleClick = (e) => {
+    e.preventDefault()
+    //(e) 確認
+    console.log(e);
+    console.log("Unko!!")
+  }
+
+  //全部のタブにイベントつける
+
+  //ボタンにhandleClick関数をくっつける
+  $tabData[0].addEventListener("click", (e) => handleClick(e))
 
 })()
