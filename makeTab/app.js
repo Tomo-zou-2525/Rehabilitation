124d8fec88a8e6a3f1bca4ba25968abc13c421d0
友造-2525 <bathbook1990tomozou@gmail.com>
2021-04-15 20:24:42 +0900

    1つめのタブの文字を表示させる

--- makeTab/app.js
+++ makeTab/app.js
@@ -1,6 +1,13 @@
-(() => {
-  // ここに処理を書く
-})()
 
-const tabItem = document.getElementById("js-tab")
-console.log(tabItem);
+
+(() => {
+  const $doc = document
+  const $tabItem = $doc.getElementById("js-tab")
+  const $tabContent = $tabItem.querySelectorAll('[data-content]')
+  //初期化＝最初に行わせたい時に行う
+  const init = () => {
+    $tabContent[0].style.display = "block"
+  }
+  init()
+
+})()
