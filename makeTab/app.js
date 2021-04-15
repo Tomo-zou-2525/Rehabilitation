19c12e491153816ff3acb8d7790c3cf07cba795e
友造-2525 <bathbook1990tomozou@gmail.com>
2021-04-15 21:25:50 +0900

    リファクタリング

--- makeTab/app.js
+++ makeTab/app.js
@@ -5,6 +5,11 @@
   const $tabItem = $doc.getElementById("js-tab")
   const $tabData = $tabItem.querySelectorAll('[data-nav]')
   const $tabContent = $tabItem.querySelectorAll('[data-content]')
+
+  //リファクタリング
+  const ACTIVE_CLASS = "is-active"
+  const navLen = $tabData.length
+
   //初期化＝最初に行わせたい時に行う
   const init = () => {
     $tabContent[0].style.display = "block"
@@ -24,23 +29,23 @@
 
     //対象外のコンテンツを無効化する
     let index = 0
-    while (index < $tabData.length) {
+    while (index < navLen) {
       //ボタンにhandleClick関数をくっつける
       $tabContent[index].style.display = "none"
-      $tabData[index].classList.remove("is-active")
+      $tabData[index].classList.remove(ACTIVE_CLASS)
       index++
     }
 
     // 対象のコンテンツをアクティブ化
     $tabItem.querySelectorAll('[data-content="' + targetVal + '"]')[0].style.display = "block"
-    $tabData[targetVal].classList.add("is-active")
+    $tabData[targetVal].classList.add(ACTIVE_CLASS)
     console.log($tabData);
 
   }
 
   //全部のタブにイベントつける
   let index = 0
-  while (index < $tabData.length) {
+  while (index < navLen) {
     //ボタンにhandleClick関数をくっつける
     $tabData[index].addEventListener("click", (e) => handleClick(e))
     index++
